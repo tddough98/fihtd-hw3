ee06ba62d6ad41eef1a7de44b11d971f35f84e66
tddough98 <40068392+tddough98@users.noreply.github.com>
2023-04-05 16:42:39 -0400

    Add notes for part 2

---
+++ part2.py
@@ -0,0 +1,17 @@
+
+# need spliced and unspliced RNA data
+
+# cellranger count --... --include-introns=True
+
+# use dataset from scVelo package for the next analysis
+
+
+# same normalization as part1.py
+
+# calculate velocity
+# interrogate individual genes
+
+# calculate full RNa dynamics
+# visualize latent time
+# investigate individual genes
+
